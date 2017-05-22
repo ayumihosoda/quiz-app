ec8bf168b013f8a04c020d71e5b1807aacd09aff
Ayumi <ayumihosoda@gmail.com>
2017-05-21 23:21:59 -0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//Questions & Answers
+var secQuestions = [
+	{
+		question: "What color were the slipper in the original Wizard of OZ?",
+		answer: "silver"
+	},
+	{
+		question: "What was the first movie ever given the title "Blockbuster"?",
+		answer: "Jaws"
+	},
+	{
+		question: "Which part of body did Charlie Chaplin insure?",
+		answer: "feet"
+	},
+	{
+		question: "What prize did Casablanca win in 1943 in the Oscars?",
+		answer: "Best film"
+	},
+	{
+		question: "Who played Chewbacca in Star Wars?",
+		answer: "Peter Mayhew"
+	},
+	{
+		question: "What was the name of the first James Bond film?",
+		answer: "Dr. No"
+	}
+	{
+		question: "Who won the most Academy Awards in acting categories?",
+		answer: "Katherine Hepburn"
+	}
+]
